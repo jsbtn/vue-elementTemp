f2e1ecd09c8e820e0529e13933902f8e9613bb99
wcz <wcz@qltyx.com>
2020-06-29 21:02:37 +0800

    ‘111

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,29 +1,28 @@
 import axios from "axios";
 import router from "@/router"
-import { getToken , setToken } from '@/utils/cookie';
+import { getToken,setToken } from '@/utils/cookie';
 import { requestData } from '@/utils/common'
-import { Toast } from 'vant';
+import { Message, Loading } from 'element-ui';
 import store from '@/store'
 
 const service = axios.create({
   timeout: 15000
 });
-let resTost = '' ;
+let resTost = '';
 service.interceptors.request.use(
   config => {
-    if(config.message){
-      const ifTost  = Toast.loading({
-        duration: 0,
-        forbidClick: true,
-        message: config.message || '请求中' ,
+    if (config.message) {
+      let loadingInstance = Loading.service({
+        background: 'rbga(0,0,0,.2)',
+        text: config.message || '请求中',
       });
     }
-    if(config.resTost){
-      resTost = config.resTost ; 
+    if (config.resTost) {
+      resTost = config.resTost;
     }
     config.headers = {
       "Content-Type": config.contentType || "application/x-www-form-urlencoded",
-      "Cookie": "edu_session="+ getToken()
+      "Cookie": "edu_session=" + getToken()
     };
     config.data = config.contentType ? config.data : requestData(config.data)
     config.params = config.contentType ? config.params : requestData(config.params)
@@ -36,31 +35,36 @@ service.interceptors.request.use(
 
 service.interceptors.response.use(
   response => {
-    Toast.clear();
+    loadingInstance.close();
     if (response.status == 200) {
       const res = response.data;
-      if (res.code === 200) { 
-        if(resTost){
-          let msg = typeof resTost == 'boolean' ? res.msg : resTost ;
-          Toast(msg) ;
-          resTost = '' ;
+      if (res.code === 200) {
+        if (resTost) {
+          let msg = typeof resTost == 'boolean' ? res.msg : resTost;
+          Message({
+            type: "success",
+            message: msg
+          });
+          resTost = '';
         }
         return Promise.resolve(res);
-      } else if(res.code == 401){
-        Toast.loading({
-          duration: 1000,
-          forbidClick: true,
-          message: res.msg ,
+      } else if (res.code == 401) {
+        Message({
+          type: "warning",
+          message: res.msg,
         });
         // 清除本地token 跳转首页
         store.dispatch('setToken', '');
         setToken("");
+        router.push({
+          path: "/"
+        });
         return Promise.resolve(res);
-      }else{
+      } else {
         return Promise.reject(response);
       }
-      resTost = '' ;
-    }else{
+      resTost = '';
+    } else {
       return Promise.reject(response);
     }
   },
