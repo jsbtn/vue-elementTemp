3247a6f5ca95b2449f53a6aded6219c77c81e6b4
wcz <wcz@qltyx.com>
2020-06-29 09:15:32 +0800

    ‘111

--- src/router/modules/index.js
+++ src/router/modules/index.js
@@ -1,35 +1,34 @@
 import Layout from '@/components/layout'
 
 const home = {
-    path: '/home',
-    component: Layout,
-    name: "index",
-    redirect: '/home/index',
-    meta: {
+  path: '/home',
+  component: Layout,
+  name: "index",
+  redirect: '/home/index',
+  meta: {
+    title: '首页',
+    icon: 'adminIndex'
+  },
+  children: [{
+      path: 'index',
+      component: () => import('@/views/home/index'),
+      name: 'index',
+      meta: {
         title: '首页',
         icon: 'adminIndex'
+      }
     },
-    children: [
-        {
-            path: 'index',
-            component: () => import('@/views/home/index'),
-            name: 'index',
-            meta: {
-                title: '首页',
-                icon: 'adminIndex'
-            }
-        },
-        { 
-            path: 'detail',
-            component: () => import('@/views/home/detail'),
-            name: 'detail',
-            meta: {
-                title: 'detail',
-                icon: 'adminabout'
-            }
-        },
+    {
+      path: 'detail',
+      component: () => import('@/views/home/detail'),
+      name: 'detail',
+      meta: {
+        title: '详情',
+        icon: 'adminabout'
+      }
+    },
 
-    ]
+  ]
 }
 
 
